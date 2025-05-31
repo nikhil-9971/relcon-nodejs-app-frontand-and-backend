be2bc989fb135bcb9e8d952ec0b8073a7cc0c055
Nikhil Trivedi <nikhiltrivedi9971@gmail.com>
2025-05-31 20:28:44 +0530

    New Logic impletemented

--- backend/routes/plans.js
+++ backend/routes/plans.js
@@ -40,7 +40,7 @@ router.get("/checkDuplicate", async (req, res) => {
 });
 
 // ✅ Get All Plans with statusSaved flag
-router.get("/getDailyPlans", async (req, res) => {
+router.get("/getDailyPlans", verifyToken, async (req, res) => {
   if (!req.session.user) return res.status(401).send("Unauthorized");
   //const { role, engineerName } = req.session.user;
   const { role, engineerName } = req.user; // 👈 Use JWT user info
