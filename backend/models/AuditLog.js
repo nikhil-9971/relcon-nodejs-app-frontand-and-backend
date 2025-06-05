672ea935ee2f32145c83ddbf21836f919e61cfed
Nikhil Trivedi <nikhiltrivedi9971@gmail.com>
2025-06-05 21:26:28 +0530

    Audit report add

---
+++ backend/models/AuditLog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const loginLogSchema = new mongoose.Schema({
+  engineerName: String,
+  username: String,
+  role: String,
+  loginTime: { type: Date, default: Date.now },
+  ip: String,
+});
+
+const auditTrailSchema = new mongoose.Schema({
+  modifiedBy: String,
+  action: String, // e.g., 'edit' or 'delete'
+  recordType: String, // e.g., 'status', 'user', etc.
+  timestamp: { type: Date, default: Date.now },
+  before: Object,
+  after: Object,
+});
+
+module.exports = {
+  LoginLog: mongoose.model("LoginLog", loginLogSchema),
+  AuditTrail: mongoose.model("AuditTrail", auditTrailSchema),
+};
