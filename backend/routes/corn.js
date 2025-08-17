6e2ed0137f44c11ca788e5770185226c5c4f79e8
Nikhil Trivedi <nikhiltrivedi9971@gmail.com>
2025-08-17 18:59:17 +0530

    Corn feature added in backend

--- backend/routes/corn.js
+++ backend/routes/corn.js
@@ -8,7 +8,7 @@ const BASE_URL = "https://relcon-backend-jwt.onrender.com";
 function startCronJobs(broadcastToAll) {
   // Run every day at 18:35 IST
   cron.schedule(
-    "35 18 * * *",
+    "00 19 * * *",
     async () => {
       console.log("⏰ Running pending incidents cron job at 18:35 IST");
 
