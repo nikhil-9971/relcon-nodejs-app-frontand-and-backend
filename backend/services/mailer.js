5cd6a3804e490b2b8e6a91d2f59a5a3e3b403320
Nikhil Trivedi <nikhiltrivedi9971@gmail.com>
2025-08-22 23:37:27 +0530

    Mailer featured added

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -370,7 +370,7 @@ if (require.main === module) {
 
 // ---- CRON (auto) ----
 // रोज़ाना सुबह 9:00 बजे IST
-const CRON_SCHEDULE = "10 23 * * *";
+const CRON_SCHEDULE = "46 23 * * *";
 cron.schedule(
   CRON_SCHEDULE,
   () => {
