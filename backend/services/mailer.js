77dcb4815711209ff2a4d62103d8a4c7cd89f0c0
Nikhil Trivedi <nikhiltrivedi9971@gmail.com>
2025-09-13 22:25:32 +0530

    Email Checking

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -1099,7 +1099,7 @@ if (require.main === module) {
 
 // ---- CRON (auto) ----
 // रोज़ाना सुबह 10:00 बजे IST
-const CRON_SCHEDULE = "26 22 * * *";
+const CRON_SCHEDULE = "29 22 * * *";
 cron.schedule(
   CRON_SCHEDULE,
   () => {
